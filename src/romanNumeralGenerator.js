385ef040d986f8ae74db073f0999a9b8c8a7001e
csikosjanos <csikosjanos@gmail.com>
2013-09-15 19:41:31 +0100

    implemented algorithm

--- src/romanNumeralGenerator.js
+++ src/romanNumeralGenerator.js
@@ -10,6 +10,20 @@ if (typeof exports === 'undefined') {
 			throw new Error('Can be generated only for number between 1 and 3999');
 		}
 
+		var digits = [
+			['','I','II','III','IV','V','VI','VII','VIII','IX'],
+			['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],
+			['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+			['','M','MM','MMM']
+		];
+
+		var ret = '';
+		var num = int.toString();
+
+		for (var i=0; i<num.length; i++) {
+			ret = digits[i][parseInt(num[num.length-(i+1)],10)] + ret;
+		}
+
 		return 'I';
 	};
 
